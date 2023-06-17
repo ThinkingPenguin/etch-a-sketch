24cf892a141c2440e1cef4da58cc21083500dba8
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-17 21:38:59 +0200

    Adding colouring of div square by clickdown on the etch-a-sketch

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-let size = 16;
+const size = 16;
+const blackColorButton = document.querySelector('black-draw');
 
 function buildChildrenDiv() {
-    const container = document.querySelector('.container');
-    const containerWidth = container.clientWidth;
-    const containerHeight = container.clientHeight;
+    let container = document.querySelector('.container');
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            const square = document.createElement('div');
+            let square = document.createElement('div');
             square.classList.add('square');
             square.style.boxSizing = "border-box"
             square.style.border = '1px black solid';
             square.style.height = `${containerHeight / size}px`;
             square.style.width = `${containerWidth / size}px`;
             container.appendChild(square);
-            
         }   
     }
+    let squareDraw = document.querySelectorAll('.square')  
+    drawing(squareDraw); 
+}
+
+function drawing(item) {
+    let changeColor = false;
+    item.forEach((cell) => {
+        cell.addEventListener('mousedown', () => {
+            cell.style.backgroundColor = 'black';
+            changeColor = true;
+        });
+
+        cell.addEventListener('mouseup', () => {
+            changeColor=false;
+        });
+
+        cell.addEventListener('mouseover', () => {
+            if(changeColor){
+                cell.style.backgroundColor = "black";
+            }
+        });
+        
+        
+    });
     
 }
 
 buildChildrenDiv();
-
