94c50c7f4670cbaf3326c76b1a09a2a476bb3257
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-19 00:16:51 +0200

    Add drawing in 'rainbow color' and clear grid when click on button, but just some problem with the code, when clicked, the first square clicked on doesn't change color, will find a solution about these problem soon

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 const size = 16;
-const blackColorButton = document.querySelector('black-draw');
+
+const btnRainbow = document.querySelector('.rainbow-color'); 
+const btnClear = document.querySelector('.erase');
+
+let isMouseDown = false;
+document.body.onmousedown = () => (isMouseDown = true);
+document.body.onmouseup = () => (isMouseDown = false);
+
+
+function takeBlackColor(cell){
+    let btnBlack = document.querySelector('.black-btn');
+    btnBlack.addEventListener("click", () => {
+        cell.addEventListener('mouseover', () => {
+            if (isMouseDown) {
+                cell.style.backgroundColor = 'black';
+            }
+        });
+    });
+}
+
+function resetGrid(cell){
+    let btnClear = document.querySelector('.erase');
+    btnClear.addEventListener("click", () => {
+        cell.style.backgroundColor = '';
+    });
+}
+
+function drawingRainbowColor(cell) {
+    let maxVal = 0xFFFFFF;
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);
+
+    btnRainbow.addEventListener("click", () => {
+        cell.addEventListener('mouseover', () => {
+            if (isMouseDown) {
+                cell.style.backgroundColor = `#${randColor.toUpperCase()}`;
+            }
+        });
+    });
+    
+}
+
 
 function buildChildrenDiv() {
     let container = document.querySelector('.container');
     let containerWidth = container.clientWidth;
     let containerHeight = container.clientHeight;
+    
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement('div');
@@ -14,33 +58,48 @@ function buildChildrenDiv() {
             square.style.height = `${containerHeight / size}px`;
             square.style.width = `${containerWidth / size}px`;
             container.appendChild(square);
+            takeBlackColor(square);
+            resetGrid(square);
+            drawingRainbowColor(square);
+            
         }   
     }
-    let squareDraw = document.querySelectorAll('.square')  
-    drawing(squareDraw); 
+    //let squareDraw = document.querySelectorAll('.square');
+
+    
+    //drawing(squareDraw); 
 }
 
+
+/*
 function drawing(item) {
-    let changeColor = false;
+    let isMouseDown = false;
+    
     item.forEach((cell) => {
         cell.addEventListener('mousedown', () => {
             cell.style.backgroundColor = 'black';
-            changeColor = true;
-        });
+            isMouseDown = true;
+        }); 
 
         cell.addEventListener('mouseup', () => {
-            changeColor=false;
+            isMouseDown=false;
         });
 
         cell.addEventListener('mouseover', () => {
-            if(changeColor){
+            if(isMouseDown){
                 cell.style.backgroundColor = "black";
             }
         });
+
+        btnClear.addEventListener('click', () => {
+            item.forEach((cell) => {
+                cell.style.backgroundColor = '';
+            });
+        })
         
         
     });
-    
-}
 
+}
+*/
 buildChildrenDiv();
